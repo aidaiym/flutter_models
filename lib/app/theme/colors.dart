49f2c170961d6ea8abd1f3a2e3cf76e8b0790cf4
aidaiym <omurbekovnaaidaiym@gmail.com>
2022-08-09 14:37:31 +0600

    added colors

---
+++ lib/app/theme/colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  /// Named Theme Color
+  static const Color primary = Color(0xffF44144);
+  static const Color onPrimary = Color(0xffFCE8E2);
+  static const Color secondary = Color.fromARGB(255, 255, 254, 254);
+  static const Color onSecondary = Colors.transparent;
+  static const Color scaffoldBackgroundColor = Color(0xffFFFFFF);
+  static const Color backgroundColor = Color(0xffE5E5E5);
+  static const Color hoverColor = Color(0xffF7F7FF);
+  static const Color cardColor = Color(0xffF2F5FF);
+  static const Color selectedRowColor = Color(0xffFFFFFF);
+  static const Color dialogBackgroundColor = Color(0xffFFFFFF);
+  static const Color focusColor = Color(0xffB9C1D3);
+  static const Color hintColor = Color(0xff525E7B);
+  static const Color disabledColor = Color(0xff525E7B);
+  static const Color canvasColor = Color(0xffF7F8FB);
+  static const Color dividerColor = Color(0xffD7DDEC);
+  static const Color shadowColor = Colors.black12;
+  static const Color scaffoldBack2 = Color.fromARGB(255, 255, 235, 235);
+
+  /// Custom colors
+  static const Color textBlack = Color(0xff000000);
+  static const Color textGrey = Color(0xff525E7B);
+  static const Color bannerBack = Color(0xffFEDADA);
+  static const Color buttonGradientSecondary = Color(0xffFF8080);
+  static const Color cardShadow = Color.fromARGB(31, 13, 7, 7);
+  static const Color fillColor = Color(0xFFF5F6F9);
+}
